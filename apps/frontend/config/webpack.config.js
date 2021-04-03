4d25bc787e479569d4cd85dae1924adea637dfe4
Patrick Roza <contact@patrickroza.com>
2021-04-03 17:35:40 +0200

    compile packages

--- apps/frontend/config/webpack.config.js
+++ apps/frontend/config/webpack.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
@@ -433,6 +433,48 @@ module.exports = function (webpackEnv) {
                 compact: isEnvProduction,
               },
             },
+            {
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
+                include: path.resolve("../../packages"),
+                loader: require.resolve('babel-loader'),
+                options: {
+                  customize: require.resolve(
+                    'babel-preset-react-app/webpack-overrides'
+                  ),
+                  presets: [
+                    [
+                      require.resolve('babel-preset-react-app'),
+                      {
+                        runtime: hasJsxRuntime ? 'automatic' : 'classic',
+                      },
+                    ],
+                  ],
+
+                  plugins: [
+                    [
+                      require.resolve('babel-plugin-named-asset-import'),
+                      {
+                        loaderMap: {
+                          svg: {
+                            ReactComponent:
+                              '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                          },
+                        },
+                      },
+                    ],
+                    isEnvDevelopment &&
+                      shouldUseReactRefresh &&
+                      require.resolve('react-refresh/babel'),
+                  ].filter(Boolean),
+                  // This is a feature of `babel-loader` for webpack (not Babel itself).
+                  // It enables caching results in ./node_modules/.cache/babel-loader/
+                  // directory for faster rebuilds.
+                  cacheDirectory: true,
+                  // See #6846 for context on why cacheCompression is disabled
+                  cacheCompression: false,
+                  compact: isEnvProduction,
+                },
+              },
             // Process any JS outside of the app with Babel.
             // Unlike the application JS, we only compile the standard ES features.
             {
