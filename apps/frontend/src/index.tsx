dafa3d1c73a698218e351015d4bbbd8a5e3e32b6
Patrick Roza <contact@patrickroza.com>
2021-04-03 18:50:12 +0200

    Convert GlobalStyle.

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <><GlobalStyle /><App /></>
   </React.StrictMode>,
   document.getElementById('root')
 );
