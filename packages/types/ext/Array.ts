d9699ba9425af0bb3f4f1813640441707be60df9
Patrick Roza <contact@patrickroza.com>
2021-04-05 14:10:33 +0200

    Cleanup

--- packages/types/ext/Array.ts
+++ packages/types/ext/Array.ts
@@ -1,14 +1,24 @@
 import * as A from "@effect-ts/core/Array"
+import { flow, Predicate } from "@effect-ts/core/Function"
 import * as O from "@effect-ts/core/Option"
 
 export function modifyAtOrOriginal_<A>(as: A.Array<A>, i: number, f: (a: A) => A) {
   return A.modifyAt_(as, i, f)["|>"](O.getOrElse(() => as))
 }
 
+export const findIndexOrElse_ = flow(
+  A.findIndex_,
+  O.getOrElse(() => -1)
+)
+
+export function findIndexOrElse<A>(predicate: Predicate<A>): (as: Array<A>) => number {
+  return (as) => findIndexOrElse_(as, predicate)
+}
+
 export function modifyOrOriginal_<A>(as: A.Array<A>, a: A, f: (a: A) => A) {
   return modifyAtOrOriginal_(
     as,
-    A.findIndex_(as, (x) => x === a)["|>"](O.getOrElse(() => -1)),
+    findIndexOrElse_(as, (x) => x === a),
     f
   )
 }
