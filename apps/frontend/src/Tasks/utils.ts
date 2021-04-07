b5d7c39d8b52228956ef92ceb015741b95e8b9b5
Patrick Roza <contact@patrickroza.com>
2021-04-07 16:48:06 +0200

    Add dbg util

--- apps/frontend/src/Tasks/utils.ts
+++ apps/frontend/src/Tasks/utils.ts
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
+
 export type WithLoading<Fnc> = Fnc & {
   loading: boolean
 }
 
 export function withLoading<Fnc>(fnc: Fnc, loading: boolean): WithLoading<Fnc> {
   return Object.assign(fnc, { loading })
+}
+
+export function useReportLoading(name: string) {
+  return useEffect(() => {
+    console.log("$$$ loaded", name)
+
+    return () => console.log("$$$ unloaded", name)
+  }, [])
 }
