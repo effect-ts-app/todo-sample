09fc89eba817ad0782ac9a48fe8f157d4176c4d8
Patrick Roza <contact@patrickroza.com>
2021-04-11 19:51:00 +0200

    Use Ref for context.

--- apps/api/Tasks/TaskContext.ts
+++ apps/api/Tasks/TaskContext.ts
@@ -4,6 +4,7 @@ import { NonEmptyString } from "@effect-ts-demo/todo-types/shared"
 import * as A from "@effect-ts/core/Collections/Immutable/Array"
 import * as Map from "@effect-ts/core/Collections/Immutable/Map"
 import * as T from "@effect-ts/core/Effect"
+import * as Ref from "@effect-ts/core/Effect/Ref"
 import { flow, pipe } from "@effect-ts/core/Function"
 import * as O from "@effect-ts/core/Option"
 import * as Sy from "@effect-ts/core/Sync"
@@ -16,29 +17,32 @@ import { NotFoundError } from "@/errors"
 
 const encodeTask = flow(strict(Task).shrink, Sy.chain(encode(Task)))
 const runEncodeTask = flow(encodeTask, Sy.run)
-let tasks: Map.Map<UUID, TaskE> = pipe(
-  [
-    Task.create({
-      title: "My first Task" as NonEmptyString,
-      steps: [Step.create({ title: "first step" as NonEmptyString })],
-    }),
-    Task.create({ title: "My second Task" as NonEmptyString, steps: [] }),
-    Task.create({
-      title: "My third Task" as NonEmptyString,
-      steps: [
-        Step.build({ title: "first step" as NonEmptyString, completed: true }),
-        Step.create({ title: "second step" as NonEmptyString }),
-      ],
-    })["|>"](Task.complete),
-  ],
-  A.map((task) => [task.id, runEncodeTask(task)] as const),
-  Map.make
+
+const tasksRef = Ref.unsafeMakeRef<Map.Map<UUID, TaskE>>(
+  pipe(
+    [
+      Task.create({
+        title: "My first Task" as NonEmptyString,
+        steps: [Step.create({ title: "first step" as NonEmptyString })],
+      }),
+      Task.create({ title: "My second Task" as NonEmptyString, steps: [] }),
+      Task.create({
+        title: "My third Task" as NonEmptyString,
+        steps: [
+          Step.build({ title: "first step" as NonEmptyString, completed: true }),
+          Step.create({ title: "second step" as NonEmptyString }),
+        ],
+      })["|>"](Task.complete),
+    ],
+    A.map((task) => [task.id, runEncodeTask(task)] as const),
+    Map.make
+  )
 )
 
 const { decode: decodeTask } = strictDecoder(Task)
 export function find(id: UUID) {
   return pipe(
-    T.effectTotal(() => O.fromNullable(tasks.get(id))),
+    tasksRef.get["|>"](T.map((tasks) => O.fromNullable(tasks.get(id)))),
     EO.chain(flow(decodeTask, EO.fromEffect, T.orDie))
   )
 }
@@ -51,19 +55,19 @@ export function get(id: UUID) {
 }
 
 export const all = pipe(
-  T.effectTotal(() => [...tasks.values()] as const),
-  T.chain(T.forEach(decodeTask)),
+  tasksRef.get["|>"](T.chain((tasks) => T.forEach_(tasks.values(), decodeTask))),
   T.orDie
 )
 
 export function add(t: Task) {
-  return T.effectTotal(() => {
-    tasks = tasks["|>"](Map.insert(t.id, runEncodeTask(t)))
-  })
+  return pipe(
+    T.structPar({ encT: encodeTask(t), tasks: tasksRef.get }),
+    T.chain(({ encT, tasks }) => tasksRef.set(tasks["|>"](Map.insert(t.id, encT))))
+  )
 }
 
 export function remove(t: Task) {
-  return T.effectTotal(() => {
-    tasks = tasks["|>"](Map.remove(t.id))
-  })
+  return tasksRef.get["|>"](
+    T.chain((tasks) => tasksRef.set(tasks["|>"](Map.remove(t.id))))
+  )
 }
