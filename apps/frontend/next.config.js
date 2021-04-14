7d75d54ec30f272882e17c7fdfe1a1df81b8a169
Patrick Roza <contact@patrickroza.com>
2021-04-14 12:03:42 +0200

    Switch to nextjs supported loader and setup

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,31 +1,25 @@
-const withTM = require('next-transpile-modules')(['fp-ts', '@nll/datum']);
+const withTM = require('next-transpile-modules')(['fp-ts', '@nll/datum', '@effect-ts-demo/todo-client', '@effect-ts-demo/todo-types']);
 
 // eslint-disable-next-line no-undef
 module.exports = withTM({
-  webpack: (config) => {
-    config.module.rules = [
-      {
-        test: /\.(tsx|ts)$/,
-        use: [
-          {
-            loader: "ts-loader",
-            options: {
-              configFile: "tsconfig.build.json",
-              compiler: "ttypescript"
-            }
-          }
-        ]
-      },
-      ...config.module.rules.map((r) => {
-        if (String(r.test).includes("ts")) {
-          return { ...r, test: /\.(js|mjs|jsx)$/ }
-        }
-        return r
-      })
-    ]
+  //webpack: (config) => {
+    //config.module.rules = [
+        // customisation for using effect-ts typescript compiler with tracing
 
-    return config
-  },
+//       {
+//         test: /\.(tsx|ts)$/,
+//         use: [
+//           {
+//             loader: "ts-loader",
+//             options: {
+//               configFile: "tsconfig.build.json",
+//               compiler: "ttypescript"
+//             }
+//           }
+//         ]
+//       },
+//     return config
+//   },
   future: {
     webpack5: true
   }
