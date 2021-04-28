ffbd56ba67f54c83acac21682b125ad50a300a55
Patrick Roza <contact@patrickroza.com>
2021-04-29 00:58:50 +0200

    Cleanup effect import

--- packages/types/ext/Effect.ts
+++ packages/types/ext/Effect.ts
@@ -1,6 +1,11 @@
 import { pipe } from "@effect-ts/core"
-import * as T from "@effect-ts/core/Effect"
-import { effectAsyncInterrupt } from "@effect-ts/core/Effect"
+import {
+  IO,
+  succeed,
+  succeedWith,
+  fail,
+  effectAsyncInterrupt,
+} from "@effect-ts/core/Effect"
 import { Lazy } from "@effect-ts/core/Function"
 import * as O from "@effect-ts/core/Option"
 
@@ -9,17 +14,17 @@ export function tryCatchPromiseWithInterrupt<E, A>(
   onReject: (reason: unknown) => E,
   canceller: () => void,
   __trace?: string
-): T.IO<E, A> {
+): IO<E, A> {
   return effectAsyncInterrupt((resolve) => {
     promise()
-      .then((x) => pipe(x, T.succeed, resolve))
-      .catch((x) => pipe(x, onReject, T.fail, resolve))
-    return T.succeedWith(canceller)
+      .then((x) => pipe(x, succeed, resolve))
+      .catch((x) => pipe(x, onReject, fail, resolve))
+    return succeedWith(canceller)
   }, __trace)
 }
 
-export function encaseOption_<E, A>(o: O.Option<A>, onError: Lazy<E>): T.IO<E, A> {
-  return O.fold_(o, () => fail(onError()), T.succeed)
+export function encaseOption_<E, A>(o: O.Option<A>, onError: Lazy<E>): IO<E, A> {
+  return O.fold_(o, () => fail(onError()), succeed)
 }
 
 export function encaseOption<E>(onError: Lazy<E>) {
