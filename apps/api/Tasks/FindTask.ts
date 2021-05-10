d27253844355abba0ceb106fcd75dc93a3ad1d89
Patrick Roza <contact@patrickroza.com>
2021-05-11 00:17:41 +0200

    Revert "do pipe test."
    This reverts commit f7706c8d8868ba31ac6181b8e75a6b6bb77ac200.

--- apps/api/Tasks/FindTask.ts
+++ apps/api/Tasks/FindTask.ts
@@ -5,24 +5,20 @@ import * as TaskContext from "./TaskContext"
 import { authorizeTask, getLoggedInUser, handle } from "./shared"
 
 import * as EO from "@effect-ts-demo/core/ext/EffectOption"
-import { identity, pipe } from "@effect-ts-demo/core/ext/Function"
+import { identity } from "@effect-ts-demo/core/ext/Function"
 import * as FindTask from "@effect-ts-demo/todo-client/Tasks/FindTask"
 
 export default handle(FindTask)((_) =>
   EO.gen(function* ($) {
     const task = yield* $(TaskContext.find(_.id))
     const { user } = yield* $(
-      pipe(
-        T.do,
-        T.bind("user", () => getLoggedInUser),
-        T.bind("taskLists", ({ user }) => TaskContext.allTaskLists(user.id)),
-        T.tap(({ taskLists, user }) =>
-          authorizeTask(taskLists).authorize_(task, user.id, identity)
-        ),
-        EO.fromEffect
-      )
+      T.gen(function* ($) {
+        const user = yield* $(getLoggedInUser)
+        const taskLists = yield* $(TaskContext.allTaskLists(user.id))
+        yield* $(authorizeTask(taskLists).authorize_(task, user.id, identity))
+        return { user }
+      })["|>"](EO.fromEffect)
     )
-
     return {
       ...task,
       myDay: user["|>"](User.getMyDay(task)),
