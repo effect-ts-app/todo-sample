33789e4f9e65b7732083aa225175ba8a3e0e8c1b
Patrick Roza <contact@patrickroza.com>
2021-05-23 12:14:32 +0200

    add helper.

--- packages/core/test.helpers.ts
+++ packages/core/test.helpers.ts
@@ -5,6 +5,9 @@ export const positiveIntUnsafe = positiveInt["|>"](S.unsafe)
 export const reasonableString = S.Constructor.for(S.reasonableString)
 export const reasonableStringUnsafe = reasonableString["|>"](S.unsafe)
 
+export const nonEmptyString = S.Constructor.for(S.nonEmptyString)
+export const nonEmptyStringUnsafe = nonEmptyString["|>"](S.unsafe)
+
 export const email = S.Constructor.for(S.Email)
 export const emailUnsafe = email["|>"](S.unsafe)
 export const phoneNumber = S.Constructor.for(S.PhoneNumber)
